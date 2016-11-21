b98878fcc9a247c6176f54b3d3ed7367344b834f
Marius Sprenger <marius.p.sprenger@gmail.com>
2016-11-21 14:59:22 +0100

    Smooth Scroll Plugin
    Smooth Scroll

--- sites/all/themes/sunrise/page.tpl.php
+++ sites/all/themes/sunrise/page.tpl.php
@@ -1,3 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+</script>
+        
+        <style>
+body, html, .main {
+    height: 100%;
+}
+
+section {
+    min-height: 100%;
+}
+</style>
+        
+    </head>
+    <body>
+
 <!-- header-banner-wrapper --> 
 <div id="header-banner-wrapper">
     <div id="header-wrapper">
@@ -399,3 +443,5 @@
 </footer>
 <!-- EOF:#subfooter -->
                     
+   </body>                 
+</html>         
