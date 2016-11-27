421d644b46a9c43863bd054d46a9138b692ac25a
michaelmika <michael.raring@gmail.com>
2016-11-27 17:21:46 +0100

    Add new Comment internal external caption

--- sites/all/themes/sunrise/comment-wrapper.tpl.php
+++ sites/all/themes/sunrise/comment-wrapper.tpl.php
@@ -9,7 +9,7 @@
   <?php print render($content['comments']); ?>
 
   <?php if ($content['comment_form']): ?>
-    <h2 class="title comment-form"><?php print t('Add new comment'); ?></h2>
+    <h2 class="title comment-form"><?php print t('Add new comment <span style="font-size:14px;">(' .$string.')</span>'; ?></h2>
     <?php print render($content['comment_form']); ?>
   <?php endif; ?>
 </section>
