dfbb3ef9abae5a8d57074e67b5a7d11cf29bbb52
michaelmika <michael.raring@gmail.com>
2016-11-27 17:15:03 +0100

    Comment intern / extern caption

--- sites/all/themes/sunrise/comment-wrapper.tpl.php
+++ sites/all/themes/sunrise/comment-wrapper.tpl.php
@@ -1,7 +1,8 @@
 <section id="comments" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if ($content['comments'] && $node->type != 'forum'): ?>
     <?php print render($title_prefix); ?>
-    <h2 class="title"><?php print t('Comments'); ?></h2>
+	<?php if (isset($_GET['intern']) && $_GET['intern']=='1'){ $string='visible for your university';}else{ $string='visible for everyone';} ?>
+    <h2 class="title"><?php print t('Comments <span style="font-size:14px;">' .$string.'</span>'); ?></h2>
     <?php print render($title_suffix); ?>
   <?php endif; ?>
 
