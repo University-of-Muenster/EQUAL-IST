d97609ed96136f222faf948030b5edd154fc2fb3
michaelmika <michael.raring@gmail.com>
2016-11-27 15:54:11 +0100

    Icons align

--- profiles/idea/modules/custom/ideal_comments/theme/comment.tpl.php
+++ profiles/idea/modules/custom/ideal_comments/theme/comment.tpl.php
@@ -72,7 +72,7 @@
      <?php
       // We hide the comments and links now so that we can render them later.
 	 
-     /* hide($content['links']);*/
+      hide($content['links']);
       print render($content); 
     ?>
       
