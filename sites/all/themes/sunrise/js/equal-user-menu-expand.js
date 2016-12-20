340b0a3550e8e27fb4f71db76bb44071b2950ca0
michaelmika <michael.raring@gmail.com>
2016-12-20 14:58:52 +0100

    Jquery Bug Undo alerts

--- sites/all/themes/sunrise/js/equal-user-menu-expand.js
+++ sites/all/themes/sunrise/js/equal-user-menu-expand.js
@@ -1,26 +1,21 @@
 (function($) { 
 	Drupal.behaviors.sunrise = {
 		attach: function(context, settings){
-			alert("Test0");
 			$(".equal-search-icon").click(function() {
 				if ($(".equal-search-block").is(":visible")){
 					$(".equal-search-block").hide("fade");	
-					alert("Test1");
 				}else{
 					$(".equal-user-menu-block").hide();
 					$(".equal-search-block").show("fade");
-					alert("Test2");
 				}
 			
 			});
 			$(".equal-user-menu-icon").click(function() {
 				if ($(".equal-user-menu-block").is(":visible")){
 					$(".equal-user-menu-block").hide("fade");			
-					alert("Test3");
 				}else{
 					$(".equal-search-block").hide();
 					$(".equal-user-menu-block").show("fade");
-					alert("Test4");
 				}
 			});
 		}
