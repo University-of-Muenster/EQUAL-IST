e38308a636c6fce89baf601d68bae9a2a277556b
michaelmika <michael.raring@gmail.com>
2016-12-20 14:55:26 +0100

    Jquery User Menu Expand Bug
    Try 1 to solve

--- sites/all/themes/sunrise/template.php
+++ sites/all/themes/sunrise/template.php
@@ -76,32 +76,17 @@ function sunrise_preprocess_html(&$variables) {
 		});',
 		array('type' => 'inline', 'scope' => 'header'));
 	}
-	/**
+		/**
 	*  Add Javascript for User menu and Search expand
 	*/
-	drupal_add_js('jQuery(document).ready(function($) { 
-		
-		$(".equal-search-icon").click(function() {
-			if ($(".equal-search-block").is(":visible")){
-				$(".equal-search-block").hide("fade");			
-			}else{
-				$(".equal-user-menu-block").hide();
-				$(".equal-search-block").show("fade");
-			}
-			
-		});
-		$(".equal-user-menu-icon").click(function() {
-			if ($(".equal-user-menu-block").is(":visible")){
-				$(".equal-user-menu-block").hide("fade");			
-			}else{
-				$(".equal-search-block").hide();
-				$(".equal-user-menu-block").show("fade");
-			}
-		});
-		
-		
-		});',
-		array('type' => 'inline', 'scope' => 'header'));
+	 drupal_add_js(
+		drupal_get_path('theme', 'sunrise') . '/js/equal-user-menu-expand.js', array( 
+			'scope' => 'header', 
+			'weight' => '15',
+			'group' => JS_LIBRARY,
+			'every_page' => TRUE			
+		));
+	
 	//EOF:Javascript
 }
 
@@ -132,6 +117,7 @@ function sunrise_preprocess_page(&$vars) {
 	} else {
 		$vars['main_grid_class'] = 'col-md-12';			
 	}
+	
 
 }
 
