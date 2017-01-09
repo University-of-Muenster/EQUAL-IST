964f2289638407423d3f422209403d84113f357b
Marius Sprenger <marius.p.sprenger@gmail.com>
2017-01-09 16:55:52 +0100

    flag

--- sites/all/themes/sunrise/page.tpl.php
+++ sites/all/themes/sunrise/page.tpl.php
@@ -354,7 +354,7 @@ section {
         <p>This project has received funding from the European Union’s Horizon 2020 research and innovation programme under grant agreement Nr.710549.</p>
             
         <br>
-          <img src="https://s-media-cache-ak0.pinimg.com/736x/20/b0/b9/20b0b92d7d8249204e12987ef105e9b9.jpg" width="50" height="50" />
+          <img src="https://europa.eu/european-union/sites/europaeu/files/docs/body/flag_yellow_low.jpg" />
         
         </div>
 </div>    
