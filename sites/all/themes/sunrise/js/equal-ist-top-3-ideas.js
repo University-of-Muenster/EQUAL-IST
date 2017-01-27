50e9d1f7bfa1d1173ba23563fc48439d8db7d169
michaelmika <michael.raring@gmail.com>
2017-01-27 19:57:11 +0100

    Top 3 ideas

--- sites/all/themes/sunrise/js/equal-ist-top-3-ideas.js
+++ sites/all/themes/sunrise/js/equal-ist-top-3-ideas.js
@@ -2,14 +2,11 @@
 
 	Drupal.behaviors.equalisttop3 = {
 		attach: function(context, settings){
-			/*
+			
 			$(".view-top-3-ideas-of-challenge .views-row-1").prepend('<div class="top3-idea-badget"><img src="/sites/all/themes/sunrise/images/goals/FirstPlace.png"></div> ');
 			$(".view-top-3-ideas-of-challenge .views-row-2").prepend('<div class="top3-idea-badget"><img src="/sites/all/themes/sunrise/images/goals/SecondPlace.png"></div> ');
 			$(".view-top-3-ideas-of-challenge .views-row-3").prepend('<div class="top3-idea-badget"><img src="/sites/all/themes/sunrise/images/goals/ThirdPlace.png"></div> ');
-			*/
-			$(".view-top-3-ideas-of-challenge .views-row-1").prepend("TEST1");
-			$(".view-top-3-ideas-of-challenge .views-row-2").prepend("TEST2");
-			$(".view-top-3-ideas-of-challenge .views-row-3").prepend("TEST3");
+			
 			
 		}
 	};	
