4c0c534e2e792dee95dea2c491b090f1b7345603
michaelmika <michael.raring@gmail.com>
2017-01-27 19:41:22 +0100

    Top 3 Ideas

---
+++ sites/all/themes/sunrise/js/equal-ist-top-3-ideas.js
@@ -0,0 +1,11 @@
+(function($) { 
+
+	Drupal.behaviors.equalistbelongingideas = {
+		attach: function(context, settings){
+			$(".view-top-3-ideas-of-challenge views-row-1").prepend('<div class="top3-idea-badget"><img src="/sites/all/themes/sunrise/images/goals/FirstPlace.png"></div> ');
+			$(".view-top-3-ideas-of-challenge views-row-2").prepend('<div class="top3-idea-badget"><img src="/sites/all/themes/sunrise/images/goals/SecondPlace.png"></div> ');
+			$(".view-top-3-ideas-of-challenge views-row-3").prepend('<div class="top3-idea-badget"><img src="/sites/all/themes/sunrise/images/goals/ThirdPlace.png"></div> ');
+		}
+	};	
+
+})(jQuery);
