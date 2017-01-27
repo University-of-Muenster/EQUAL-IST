851a71672aa44e64ec0108d57bca29eb51f026f0
michaelmika <michael.raring@gmail.com>
2017-01-27 19:56:03 +0100

    Top 3 ideas test

--- sites/all/themes/sunrise/js/equal-ist-top-3-ideas.js
+++ sites/all/themes/sunrise/js/equal-ist-top-3-ideas.js
@@ -1,6 +1,6 @@
 (function($) { 
 
-	Drupal.behaviors.equalistbelongingideas = {
+	Drupal.behaviors.equalisttop3 = {
 		attach: function(context, settings){
 			/*
 			$(".view-top-3-ideas-of-challenge .views-row-1").prepend('<div class="top3-idea-badget"><img src="/sites/all/themes/sunrise/images/goals/FirstPlace.png"></div> ');
