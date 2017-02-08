6547f5712886b1f38bf8001b93f02bca5ec3e98a
michaelmika <michael.raring@gmail.com>
2017-02-08 19:24:54 +0100

    Javascripts sorted to modules

--- sites/all/themes/sunrise/js/equal-ist-blocks.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  Drupal.behaviors.ideaexplanation = {
-  	attach: function(context, settings){
-  		$("#related-idea-explanation-link").click(function(){
-  			$("#related-idea-explanation").slideToggle("fast");
-  		});
-  	}
-  }
-}(jQuery));
